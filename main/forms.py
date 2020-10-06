6d3ec226fcf1010d9a30854a3a61c1632668d42d
amurakho <amurakho@gmail.com>
2020-10-06 13:17:17 +0300

    add disabled toe scrappers adn initial to all_scrappers field

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,6 @@ from django.conf import settings
 class ScrapperLauncherForm(forms.Form):
     SCRAPPERS_CHOICE = ((scrapper, scrapper) for scrapper in settings.SCRAPPERS)
 
-    all_scrappers = forms.BooleanField(widget=forms.CheckboxInput)
-    scrappers = forms.ChoiceField(choices=SCRAPPERS_CHOICE)
+    all_scrappers = forms.BooleanField(widget=forms.CheckboxInput, initial=True)
+    scrappers = forms.ChoiceField(choices=SCRAPPERS_CHOICE, disabled=True)
     keywords = forms.CharField(help_text='Write keywords separated by comma(",")')
